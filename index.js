056a920854f54f4101cc3ecc77e43390d89bb71a
marvinbra <marvinbopp37@gmail.com>
2023-08-24 13:50:46 +0200

    medium screen sizes update
    responsiveness

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-// javascript
-const menuBtn = document.querySelector(".mobile-nav-toggle")
-const nav = document.querySelector(".primary-navigation");
+const nav = document.querySelector(".mobile-nav-toggle")
+const navToggle = document.querySelector(".primary-navigation");
 
 menuBtn.addEventListener("click", () => {
     const visibility = nav.getAttribute("data-visibility")
     if (visibility === "false") {
         nav.setAttribute("data-visibility", true)
+        navToggle.setAttribute("aria-expanded", true)
     } else {
         nav.setAttribute("data-visibility", false)
+        navToggle.setAttribute("aria-expanded", false)
     }
-    console.log(visibility)
 })
